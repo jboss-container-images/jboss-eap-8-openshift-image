1bcc8fecb0cea90d4706933d58e6b5001067deb2
JF Denise <jdenise@redhat.com>
2022-09-07 16:37:07 +0200

    Make examples Jakarta EE10

---
+++ examples/eap/docker-build/src/main/java/org/jboss/eap/demo/jaxrs/RestApplication.java
@@ -0,0 +1,8 @@
+package org.jboss.eap.demo.jaxrs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
